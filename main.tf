2e13643979c448ab88427b7ea649c1ad4ab805ad
Rory McNicholl <rory.mcnicholl@london.ac.uk>
2020-05-07 23:52:03 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -93,9 +93,10 @@ resource "azurerm_automation_account" "default" {
   name = "${module.labels.id}"
   resource_group_name = "${azurerm_resource_group.default.name}"
   location = "${var.location}"
-  sku {
-    name = "Basic"
-  }
+  sku_name = "Basic"
+#  sku {
+#    name = "Basic"
+#  }
 
   tags = "${module.labels.tags}"
 }
