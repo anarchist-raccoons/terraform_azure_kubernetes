00df2c96da34952329bc30225fdd74fc5bab010d
Rory McNicholl <rory.mcnicholl@london.ac.uk>
2020-12-04 11:06:53 +0000

    stop using random string in SA name for now

--- main.tf
+++ main.tf
@@ -79,7 +79,7 @@ resource "random_string" "default" {
 
 # Storage Account
 resource "azurerm_storage_account" "default" {
-  name = "${module.labels.organization}${module.labels.environment}${module.labels.name}${random_string.default.result}"
+  name = "${module.labels.organization}${module.labels.environment}${module.labels.name}"
   resource_group_name = "${azurerm_resource_group.default.name}"
   location = "${var.location}"
   account_tier = "${var.account_tier}"
