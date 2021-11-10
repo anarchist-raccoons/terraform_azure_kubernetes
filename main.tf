62fa0ac31237db8bb8aa25c10dcd528047dbf09f
Rory McNicholl <rory.mcnicholl@london.ac.uk>
2021-11-10 18:05:30 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -48,7 +48,7 @@ resource "azurerm_kubernetes_cluster" "default" {
   }
   
   network_profile {
-    netwrok_plugin = "azure"
+    network_plugin = "azure"
     load_balancer_sku = "Standard"
   }
   
