5a0672487e454c86d6a3e7aa86a7f06123d6fa44
Rory McNicholl <rory.mcnicholl@london.ac.uk>
2022-06-17 13:24:14 +0000

    remove Interpolation-only expressions

--- main.tf
+++ main.tf
@@ -13,39 +13,39 @@ module "labels" {
 #  version = "0.2.1"
 
   # Required
-  environment = "${var.environment}"
-  name = "${var.name}"
+  environment = var.environment
+  name = var.name
   # Optional
-  namespace-org = "${var.namespace-org}"
-  organization = "${var.org}"
+  namespace-org = var.namespace-org
+  organization = var.org
   delimiter = "-"
-  owner = "${var.owner}"
-  team = "${var.team}"
+  owner = var.owner
+  team = var.team
   tags = {
-    Name = "${module.labels.id}"
+    Name = module.labels.id
   }
 }
 
 # Azure Resource Group
 resource "azurerm_resource_group" "default" {
-  name = "${module.labels.id}"
-  location = "${var.location}"
+  name = module.labels.id
+  location = var.location
 
-  tags = "${module.labels.tags}"
+  tags = module.labels.tags
 }
 
 # Kubernetes Cluster
 resource "azurerm_kubernetes_cluster" "default" {
-  name = "${module.labels.id}"
-  location = "${var.location}"
-  resource_group_name = "${azurerm_resource_group.default.name}"
-  dns_prefix = "${module.labels.name}" # @todo check this
+  name = module.labels.id
+  location = var.location
+  resource_group_name = azurerm_resource_group.default.name
+  dns_prefix = module.labels.name # @todo check this
 
   default_node_pool {
     name       = "default"
-    node_count = "${var.agent_count}"
-    vm_size    = "${var.vm_size}"
-    os_disk_size_gb = "${var.disk_size_gb}"
+    node_count = var.agent_count
+    vm_size    = var.vm_size
+    os_disk_size_gb = var.disk_size_gb
   }
   
   network_profile {
@@ -54,10 +54,10 @@ resource "azurerm_kubernetes_cluster" "default" {
   }
   
   linux_profile {
-    admin_username = "${var.admin_user}"
+    admin_username = var.admin_user
 
     ssh_key {
-      key_data = "${var.ssh_key}"
+      key_data = var.ssh_key
     }
   }
   
@@ -68,11 +68,11 @@ resource "azurerm_kubernetes_cluster" "default" {
   }
 
   service_principal {
-    client_id = "${var.client_id}"
-    client_secret = "${var.client_secret}"
+    client_id = var.client_id
+    client_secret = var.client_secret
   }
   
-  tags = "${module.labels.tags}"
+  tags = module.labels.tags
 }
   
 resource "random_string" "default" {
@@ -83,96 +83,96 @@ resource "random_string" "default" {
 
 # Storage Account
 resource "azurerm_storage_account" "default" {
-  name = "${module.labels.organization}${module.labels.environment}${module.labels.name}"
-  resource_group_name = "${azurerm_resource_group.default.name}"
-  location = "${var.location}"
-  account_tier = "${var.account_tier}"
-  account_replication_type = "${var.account_replication_type}"
+  name = module.labels.organization}${module.labels.environment}${module.labels.name
+  resource_group_name = azurerm_resource_group.default.name
+  location = var.location
+  account_tier = var.account_tier
+  account_replication_type = var.account_replication_type
 }
 
 # Vault (not within terraform so we can persist our vaults beyond a terraform destroy)
 #resource "azurerm_recovery_services_vault" "vault" {
 #  name = "${module.labels.organization}${module.labels.environment}${module.labels.name}-vault"
-#  resource_group_name = "${azurerm_resource_group.default.name}"
-#  location = "${var.location}"
+#  resource_group_name = azurerm_resource_group.default.name
+#  location = var.location
 #  sku = "Standard"
 #}
   
 # Container Registry
 resource "azurerm_container_registry" "default" {
-  name = "${azurerm_storage_account.default.name}"
-  resource_group_name = "${azurerm_resource_group.default.name}"
-  location = "${var.location}"
-  sku = "${var.account_tier}"
+  name = azurerm_storage_account.default.name
+  resource_group_name = azurerm_resource_group.default.name
+  location = var.location
+  sku = var.account_tier
   admin_enabled = true # registry name as username, admin user access key as password for docker (alt to IAM beloe)
 }
 
 # Azure Share
 resource "azurerm_storage_share" "default" {
-  name = "${module.labels.organization}${module.labels.environment}${module.labels.name}"
-  storage_account_name = "${azurerm_storage_account.default.name}"
+  name = module.labels.organization}${module.labels.environment}${module.labels.name
+  storage_account_name = azurerm_storage_account.default.name
   quota = 5120
 }
 
 # Backup for Azure Share
 #resource "azurerm_backup_container_storage_account" "protection-container" {
-#  resource_group_name = "${azurerm_resource_group.default.name}"
-#  recovery_vault_name = "${azurerm_recovery_services_vault.vault.name}"
-#  storage_account_id  = "${azurerm_storage_account.default.id}"
+#  resource_group_name = azurerm_resource_group.default.name
+#  recovery_vault_name = azurerm_recovery_services_vault.vault.name
+#  storage_account_id  = azurerm_storage_account.default.id
 #}
 
 # Backup Azure Fileshare polict (not within terraform so we can persist our vaults beyond a terraform destroy)
 #resource "azurerm_backup_policy_file_share" "default" {
 #  name = "${module.labels.name}-recovery-vault-policy"
-#  resource_group_name = "${azurerm_resource_group.default.name}"
-#  recovery_vault_name = "${azurerm_recovery_services_vault.vault.name}"
+#  resource_group_name = azurerm_resource_group.default.name
+#  recovery_vault_name = azurerm_recovery_services_vault.vault.name
 #  
 #  # Very simple daily backup for (defaults to 23:00 for 10 days)
 #  backup {
 #    frequency = "Daily"
-#    time      = "${var.backup_time}"
+#    time      = var.backup_time
 #  }
 #
 #  retention_daily {
-#    count = "${var.retention_count}"
+#    count = var.retention_count
 #  }
 #}
 
 # Backup Azure Fileshare 
 #resource "azurerm_backup_protected_file_share" "share1" {
-#  resource_group_name       = "${azurerm_resource_group.default.name}"
-#  recovery_vault_name       = "${azurerm_recovery_services_vault.vault.name}"
-#  source_storage_account_id = "${azurerm_backup_container_storage_account.protection-container.storage_account_id}"
-#  source_file_share_name    = "${azurerm_storage_share.default.name}"
-#  backup_policy_id          = "${azurerm_backup_policy_file_share.default.id}"
+#  resource_group_name       = azurerm_resource_group.default.name
+#  recovery_vault_name       = azurerm_recovery_services_vault.vault.name
+#  source_storage_account_id = azurerm_backup_container_storage_account.protection-container.storage_account_id
+#  source_file_share_name    = azurerm_storage_share.default.name
+#  backup_policy_id          = azurerm_backup_policy_file_share.default.id
 #}
   
 
 # Automation Account (used for start|stop)
 #resource "azurerm_automation_account" "default" {
-#  name = "${module.labels.id}"
-#  resource_group_name = "${azurerm_resource_group.default.name}"
-#  location = "${var.location}"
+#  name = module.labels.id
+#  resource_group_name = azurerm_resource_group.default.name
+#  location = var.location
 #  sku_name = "Basic"
 ##  sku {
 ##    name = "Basic"
 ##  }
 #
-#  tags = "${module.labels.tags}"
+#  tags = module.labels.tags
 #}
   
 # Log Analytics Workspace (used for start|stop)
 #resource "azurerm_log_analytics_workspace" "default" {
-#  name = "${module.labels.id}"
-#  resource_group_name = "${azurerm_resource_group.default.name}"
-#  location = "${var.location}"
+#  name = module.labels.id
+#  resource_group_name = azurerm_resource_group.default.name
+#  location = var.location
 #  sku                 = "PerGB2018"
 #  retention_in_days   = 30
 #}
 
 # Storage Account Token
 # data "azurerm_storage_account_sas" "default" {
-#   connection_string = "${azurerm_storage_account.default.primary_connection_string}"
+#   connection_string = azurerm_storage_account.default.primary_connection_string
 #   https_only = false
 
 #   resource_types {
@@ -188,9 +188,9 @@ resource "azurerm_storage_share" "default" {
 #     file = true
 #   }
 
-#   start = "${timestamp()}"
+#   start = timestamp()
 #   expiry = "${timeadd(
-#     "${timestamp()}", "360m"
+#     timestamp(), "360m"
 #     )}"
 
 #   permissions {
