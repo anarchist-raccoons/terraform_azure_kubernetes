4cea3ee661d9910fe78289ff3deea19b5b97e092
Rory McNicholl <rory.mcnicholl@london.ac.uk>
2022-06-18 21:19:25 +0000

    mark sensitive output as sensitive

--- output.tf
+++ output.tf
@@ -1,28 +1,36 @@
 
 output "kube_config" {
   value = azurerm_kubernetes_cluster.default.kube_config_raw
+  sensitive = true
 }
 output "host" {
   value = azurerm_kubernetes_cluster.default.kube_config.0.host
+  sensitive = true
 }
 output "username" {
   value = azurerm_kubernetes_cluster.default.kube_config.0.username
+  sensitive = true
 }
 output "password" {
   value = azurerm_kubernetes_cluster.default.kube_config.0.password
+  sensitive = true
 }
 output "client_certificate" {
   value = base64decode(azurerm_kubernetes_cluster.default.kube_config.0.client_certificate)
+  sensitive = true
 }
 output "client_key" {
   value = base64decode(azurerm_kubernetes_cluster.default.kube_config.0.client_key)
+  sensitive = true
 }
 output "cluster_ca_certificate" {
   value = base64decode(azurerm_kubernetes_cluster.default.kube_config.0.cluster_ca_certificate)
+  sensitive = true
 }
 
 output "azure_storage_account_key" {
   value = azurerm_storage_account.default.primary_access_key
+  sensitive = true
 }
 
 output "azure_resource_group_name" {
@@ -55,6 +63,7 @@ output "azure_container_registry_admin_username" {
 
 output "azure_container_registry_admin_password" {
   value = azurerm_container_registry.default.admin_password
+  sensitive = true
 }
 
 output "azure_cluster_node_resource_group" {
