f8712fd77327ca59d7dd4aac7fc4bec2dce57cd6
Rory McNicholl <rory.mcnicholl@london.ac.uk>
2024-05-27 20:02:04 +0100

    turn on image_cleaner

--- main.tf
+++ main.tf
@@ -40,6 +40,7 @@ resource "azurerm_kubernetes_cluster" "default" {
   location = var.location
   resource_group_name = azurerm_resource_group.default.name
   dns_prefix = module.labels.name # @todo check this
+  image_cleaner_enabled = true
 
   default_node_pool {
     name       = "default"
